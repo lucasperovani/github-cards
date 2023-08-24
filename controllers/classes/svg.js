8cad34d00611b992a9661e934df9df36f74d83c3
Lucas Perovani <lucasperovani@gmail.com>
2023-08-24 04:16:36 +0000

    Created svg title element

--- controllers/classes/svg.js
+++ controllers/classes/svg.js
@@ -224,5 +224,57 @@ class BasicSVG extends SVG {
     }
 };
 
+
+/**
+ * Create a SVG title.
+ *
+ * @param {String} title - The title.
+ *
+ * @return {SVG} The SVG title.
+ */
+class SVGTitle extends SVG {
+    /**
+     * This function creates a new SVGTitle object.
+     *
+     * @param {String} title - The title.
+     *
+     * @override
+     * @see SVG#constructor
+     */
+    constructor(title = '') {
+        // Call the parent constructor
+        super();
+
+        // If there is an error, return
+        if (this.error) return;
+
+        // If the title is not a string, return
+        if (typeof title !== 'string') {
+            this.error = true;
+            this.message = 'Invalid title';
+
+            return;
+        }
+
+        // Set the title
+        this.title = title;
+    }
+
+    /**
+     * This function returns the SVG string.
+     *
+     * @return {String} The SVG string.
+     *
+     * @override
+     * @see SVG#build
+     */
+    build() {
+        // Return the SVG string
+        return '<title' + this.buildParameters() + '>' +
+                    this.title +
+                '</title>';
+    }
+};
+
 // Export SVGs class
 module.exports = {SVG, BasicSVG};

bf202983d05eae6eec857b910492bc26cecad056
Lucas Dormea <lucasperovani@gmail.com>
2023-09-13 00:29:13 -0300

    Improved parameter logic.

--- controllers/svg_parameters/parameter.js
+++ controllers/svg_parameters/parameter.js
@@ -1,3 +1,6 @@
+const {STRING_REGEX} = require('../constants.js');
+
+
 /**
  * This class represents an SVG Parameter.
  * It is used to build an SVG Parameter string.
@@ -12,17 +15,17 @@
  * @returns {Parameter} The SVG parameter.
  */
 class Parameter {
-    /**
+	/**
 	* This function creates a new SVG Parameter object.
 	*/
 	constructor() {
-        this.error = false;
-        this.message = 'OK';
-        this.parameterName = 'Parameter';
-        this.value = '';
-    }
+		this.error = false;
+		this.message = 'OK';
+		this.parameterName = 'Parameter';
+		this.value = '';
+	}
 
-    /**
+	/**
 	 * Get the error status.
 	 *
 	 * @returns {Boolean} The error status.
@@ -31,42 +34,50 @@ class Parameter {
 		return {error: this.error, message: this.message};
 	}
 
-    /**
-     * Get the parameter name.
-     *
+	/**
+	 * Get the parameter name.
+	 *
 	 * @returns {String} This parameter name.
-     */
-    getParameterName() {
-        return this.parameterName;
-    }
+	 */
+	getParameterName() {
+		return this.parameterName;
+	}
 
-    /**
-     * Set the parameter value.
-     *
-     * @param {String} value - The parameter value.
+	/**
+	 * Set the parameter value.
+	 *
+	 * @param {String} value - The parameter value.
 	 *
 	 * @returns {Boolean} If the value is valid.
-     */
-    setParameterValue(value) {
-        if (typeof value !== 'string' || value === '') {
-            this.error = true;
-            this.message = 'Invalid value';
-            return;
-        }
+	 */
+	setParameterValue(value) {
+		if (
+			typeof value !== 'string' || value === '' ||
+			!STRING_REGEX.test(value)
+		) {
+			this.error = true;
+			this.message = 'Invalid value';
+			return false;
+		}
 
-        this.value = value;
-    }
+		this.value = value;
+		return true;
+	}
 
-    /**
-     * Build this parameter, returning a string.
-     *
+	/**
+	 * Build this parameter, returning a string.
+	 *
 	 * @returns {String} The SVG Parameter string.
-     */
-    build() {
-        if (this.error)  return '';
+	 */
+	build() {
+		// Return if there is an error, or if the value is not a string
+		// or the string is invalid
+		if (
+			this.error || !this.value || !STRING_REGEX.test(this.value)
+		) return '';
 
-        return '';
-    }
+		return '';
+	}
 };
 
 
